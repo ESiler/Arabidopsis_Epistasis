daa859a2d09c8d1ac612a2ea4d8d25ed262568e4
ESiler <eleanor.siler@gmail.com>
2023-08-17 17:49:43 -0400

    Now it has data comparing all the traits and mutants and whether the plants traits differ between each comparison. Next step is to visualize this.

--- scripts/99_barplots_and_interaction_type_summary.R
+++ scripts/99_barplots_and_interaction_type_summary.R
@@ -1,5 +1,35 @@
 source('scripts/load_required_packages.R')
 load("rdata/03_workspace.RData")
 
-str(data)
-head(data)
+df = r.df.all_results
+#Add six columns: AvsWT, BvsWT, ABvsWT, ABvsA, ABvsB, BvsA 
+#For each column and each row it should tell you if the fitness is higher (1), lower (-1), or nonsignificant(0)
+#Define function for getting fitness comparison value:
+get_effect = function(X_lci, X_uci, Y_lci, Y_uci){
+  if (X_uci < Y_lci) {
+    dif=-1}
+  else if (X_lci > Y_uci) {
+    dif=1}
+  else if ((X_uci >= Y_lci) & (X_lci <= Y_uci)){
+    dif = 0}
+  else{dif=NA
+  print("invalid input?")}
+  return(dif)
+}
+##It turns out you need to "vectorize" udfs for them to work with mutate. Who knew?
+get_effect <- Vectorize(get_effect)
+
+#Add comparison columns
+df <- df %>% mutate(A_vs_WT = get_effect(MA_w_lci, MA_w_uci, WT_w_lci, WT_w_uci))
+df <- df %>% mutate(B_vs_WT = get_effect(MB_w_lci, MB_w_uci, WT_w_lci, WT_w_uci))
+df <- df %>% mutate(AB_vs_WT = get_effect(DM_w_lci, DM_w_uci, WT_w_lci, WT_w_uci))
+df <- df %>% mutate(AB_vs_A = get_effect(DM_w_lci, DM_w_uci, MA_w_lci, MA_w_uci))
+df <- df %>% mutate(AB_vs_B = get_effect(DM_w_lci, DM_w_uci, MB_w_lci, MB_w_uci))
+df <- df %>% mutate(A_vs_B = get_effect(MA_w_lci, MA_w_uci, MB_w_lci, MB_w_uci))
+
+#Make effect comparison dataframe
+effect_comp_df <- df[, c(1, (ncol(df) - 6):ncol(df))]
+
+effect_comp_df <- effect_comp_df %>% arrange(trait, A_vs_WT, B_vs_WT, AB_vs_WT, AB_vs_A, AB_vs_B, A_vs_B)
+head(effect_comp_df)
+
